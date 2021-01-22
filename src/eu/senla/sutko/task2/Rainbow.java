d70ecf9ce6e649eb5ce403af25ad548d1bceb5bd
SUTIK <SUTIK90@MAIL.RU>
2021-01-22 11:37:05 +0300

    создал переменные и метод

--- src/eu/senla/sutko/task2/Rainbow.java
+++ src/eu/senla/sutko/task2/Rainbow.java
@@ -13,8 +13,25 @@ package eu.senla.sutko.task2;
             case 2:
                 System.out.println("ORANGE");
                 break;
-                
+            case 3:
+                System.out.println("YELLOW");
+                break;
+            case 4:
+                System.out.println("GREEN");
+                break;
+            case 5:
+                System.out.println("SKY");
+                break;
+            case 6:
+                System.out.println("BLUE");
+                break;
+            case 7:
+                System.out.println("FIOLET");
+                break;
             default:
+                System.out.println("Введите цифру от 1 до 7");
+
+
         }
     }
 
